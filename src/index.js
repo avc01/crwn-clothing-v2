5b9136660c1492fb14f4d3e0b2b7a6d2a72809de
Andrés Víquez <73793020+avc01@users.noreply.github.com>
2022-03-24 21:57:36 -0600

    navigation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.scss";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
