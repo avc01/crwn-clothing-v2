95b01af5a257124b893a8e180928cfc4abd86571
Andrés Víquez <73793020+avc01@users.noreply.github.com>
2022-07-05 21:13:10 -0600

    categories saga into typescript

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,5 +1,5 @@
 // Imports
-import { takeLatest, all, call, put } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "typed-redux-saga";
 
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
@@ -11,10 +11,10 @@ import {
 // Saga Generators
 function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocuments);
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
     yield put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield put(fetchCategoriesFailed(error as Error));
   }
 }
 
